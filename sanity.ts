eba406956f87e6da67ca552d91f1c35b2473e320
mohamed-abdulla <abdabdulla78@gmail.com>
2023-01-26 12:04:00 +0530

    apiversion

--- sanity.ts
+++ sanity.ts
@@ -3,13 +3,12 @@ import createImageUrlBuilder from "@sanity/image-url";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "production"; // "pv8y60vp"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-const apiVersion = "2023-01-1"; // "2022-11-16"
 const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
 
 export const config = {
   projectId,
   dataset,
-  apiVersion,
+  apiVersion: "2023-01-1",
   useCdn: process.env.NODE_ENV === "production",
   token: token,
 };
